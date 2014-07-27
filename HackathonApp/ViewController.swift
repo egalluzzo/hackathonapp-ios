b15a8147f597f35484bb139fa8626ccb13d7f192
Eric Galluzzo <egalluzzo@gmail.com>
2014-07-26 21:12:36 -0400

    Initial Commit

---
+++ HackathonApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackathonApp
+//
+//  Created by Eric Galluzzo on 7/26/14.
+//  Copyright (c) 2014 Eric Galluzzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HackathonAppTests/HackathonAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  HackathonAppTests.swift
+//  HackathonAppTests
+//
+//  Created by Eric Galluzzo on 7/26/14.
+//  Copyright (c) 2014 Eric Galluzzo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class HackathonAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
